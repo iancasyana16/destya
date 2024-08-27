044518d509660fb7f19e4444436e4c98c19ee8fa
iancasyana16 <iancasyana@gmail.com>
2024-08-27 10:40:54 +0700

    fix win

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-// onload = () =>{
-//     var musik = "";
+onload = () =>{
+    var musik = "";
 
-//     var audio = document.getElementById("#myAudio");
-//     if (musik) {
-//         audio.src = musik;
-//     }
-//     document.body.classList.remove("container"); 
-// };
+    var audio = document.getElementById("#myAudio");
+    if (musik) {
+        audio.src = musik;
+    }
+    document.body.classList.remove("container"); 
+};
+
+// Function to play audio
+function playAudio() {
+    var audio = document.getElementById('myAudio');
+    audio.play();
+}
